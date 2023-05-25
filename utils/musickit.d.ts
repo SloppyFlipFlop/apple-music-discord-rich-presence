56df2da34b3dbf0c80b85de298a390fd8906c302
David Nevarez <david.nevarez03@hotmail.com>
2023-05-25 06:54:28 -0700

    current app state
    just pushing what i have so far

---
+++ utils/musickit.d.ts
@@ -0,0 +1,20 @@
+// declare namespace MusicKit {
+//   interface App {
+//     name: string;
+//     build: string;
+//   }
+
+//   interface Configuration {
+//     developerToken: string;
+//     app: App;
+//   }
+
+//   function configure(configuration: Configuration): void;
+
+//   interface Music {
+//     authorize(): Promise<string>;
+//   }
+// }
+
+// declare const MusicKit: MusicKit.Music;
+// export default MusicKit;
